bde0636ee35694f6bb2732607e08a5025f167b35
BOUHOUCH chamseddine <bouhouchchamseddine@gmail.com>
2021-03-10 18:34:38 +0100

    first commit

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        Bye Dora !
+    </div>
+</template>
+<script>
+export default {
+    name:'App',
+}
+</script>
+<style>
+    #app{
+        text-align:center;
+    }
+</style>
